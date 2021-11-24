0475f2909aed2b987d60f7f0ae7e1c8a35e48684
Karent Saenz <92121406+karentmt1809@users.noreply.github.com>
2021-11-24 00:51:14 -0500

    Add files via upload

---
+++ reto1/reto1/src/main/java/com/retomt1/reto1/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.retomt1.reto1.controller;
+
+import java.util.List;
+
+import com.retomt1.reto1.model.User;
+import com.retomt1.reto1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author karent_saenz
+ */
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin("*")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+    
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registrar(@RequestBody User user) {
+        return userService.registrar(user);
+    }
+    
+    @GetMapping("/{email}/{password}")
+    public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password) {
+        return userService.autenticarUsuario(email, password);
+    }
+    
+    @GetMapping("/{email}")
+    public boolean existeEmail(@PathVariable("email") String email) {
+        return userService.existeEmail(email);
+    }   
+}
